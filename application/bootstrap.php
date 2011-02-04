93eff58614bcbc511698e9abbf97044dbda7704b
smgladkovskiy <SMGladkovskiy@gmail.com>
2011-02-04 14:59:58 +0300

    multilanguage and comingsoon tracing

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -53,7 +53,7 @@ ini_set('unserialize_callback_func', 'spl_autoload_call');
 /**
  * Set the default language
  */
-I18n::lang('en-us');
+I18n::lang(key(Request::accept_lang()));
 
 /**
  * Set Kohana::$environment if a 'KOHANA_ENV' environment variable has been supplied.
