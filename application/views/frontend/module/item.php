30349c65f445a74a670546213e31d00231fc9057
Aleksey Suleymanov <aleksey.suleimanov@gmail.com>
2011-03-15 14:39:24 +0300

    Edit view item and css file

--- application/views/frontend/module/item.php
+++ application/views/frontend/module/item.php
@@ -1,15 +1,21 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');?>
-<div>
+<div class="module_head">
 	<?php echo html::anchor(
 		Route::url('modules', array(
 			'developer' => $module->developer->username,
 			'module' => $module->name
 		)),
-		$module->name);
+			'<h1>'.$module->name.'</h1>',
+			array('style' => 'font-size:1.7em')
+		);
 	?>
 </div>
-<div></div>
-<div>
+<div class="module_desc">
+	<?php
+		echo $module->description;
+	?>
+</div>
+<div class="module_foot">
 	<?php echo html::anchor(
 		Route::url('modules', array(
 			'developer' => $module->developer->username
