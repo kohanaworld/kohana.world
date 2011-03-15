8edf7c766063f6c0891a07a948a3f933e79d7f9a
Aleksey Suleymanov <aleksey.suleimanov@gmail.com>
2011-03-16 00:27:25 +0300

    Final edit view "item.php". Add css-style from pagination.

--- application/views/frontend/module/item.php
+++ application/views/frontend/module/item.php
@@ -1,23 +1,18 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');?>
 <div class="module_head">
 	<div class="module_title">
-				<?php echo html::anchor(
+		<?php echo html::anchor(
 			Route::url('modules', array(
 				'developer' => $module->developer->username
 			)),
 			$module->developer->username);
-		?> /
-		<?php echo html::anchor(
-			Route::url('modules', array(
-				'developer' => $module->developer->username,
-				'module' => $module->name
-			)),
-				$module->name);
-		?>
-	</div>
-	<div class="module_create">
-		<?php
-			echo date('d-M-Y',$module->created_at);
+			echo ' / ';
+			echo html::anchor(
+				Route::url('modules', array(
+					'developer' => $module->developer->username,
+					'module' => $module->name
+				)),
+					$module->name);
 		?>
 	</div>
 </div>
@@ -27,5 +22,17 @@
 	?>
 </div>
 <div class="module_foot">
-
+	<div class="module_github">
+		<span class="gst">Github Info : </span>
+			<?php
+				echo '<ul>';
+				echo '<li class="mod_create">'.html::anchor($module->url,date('d-M-Y',$module->created_at),array('target'=>'_blank')).'</li>';
+				echo '<li class="mod_watch">'.html::anchor($module->url.'/watchers',$module->info->watchers,array('target'=>'_blank')).'</li>';
+				echo '<li class="mod_forks">'.html::anchor($module->url.'/network',$module->info->forks,array('target'=>'_blank')).'</li>';
+				echo '</ul>';
+			?>
+	</div>
+	<div class="module_site">
+		<span class="gst">Site Info : </span>
+	</div>
 </div>
