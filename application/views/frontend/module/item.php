a8104bb48bc54c9722ff29fd25eb72aeaa1ad7d1
Aleksey Suleymanov <aleksey.suleimanov@gmail.com>
2011-03-17 13:48:18 +0300

    Re-edit item.php file

--- application/views/frontend/module/item.php
+++ application/views/frontend/module/item.php
@@ -14,6 +14,13 @@
 				$module->name);
 	?>
 	<span>All Ratings Data</span>
+	<?php
+		echo '<ul>';
+		echo '<li class="mod_github">'.html::anchor($module->url,'&nbsp;',array('target'=>'_blank','title'=>'GitHub.com Info')).'</li>';
+		echo '<li class="mod_watch">'.html::anchor($module->url.'/watchers',$module->info->watchers,array('target'=>'_blank','title'=>__('Watchers'))).'</li>';
+		echo '<li class="mod_forks">'.html::anchor($module->url.'/network',$module->info->forks,array('target'=>'_blank','title'=>__('Forks'))).'</li>';
+		echo '</ul>';
+	?>
 </div>
 <div class="module_desc">
 	<?php
@@ -23,11 +30,6 @@
 <div class="module_foot">
 	<?php
 		echo '<ul>';
-		echo '<li class="mod_github">'.html::anchor($module->url,'&nbsp;',array('target'=>'_blank','title'=>'GitHub.com Info')).'</li>';
-		echo '<li class="mod_watch">'.html::anchor($module->url.'/watchers',$module->info->watchers,array('target'=>'_blank','title'=>__('Watchers'))).'</li>';
-		echo '<li class="mod_forks">'.html::anchor($module->url.'/network',$module->info->forks,array('target'=>'_blank','title'=>__('Forks'))).'</li>';
-		echo '</ul>';
-		echo '<ul style="float:right;">';
 		echo '<li class="gst">Site Info : </li>';
 		echo '<li class="mod_cat">'.html::anchor('#','Category List',array('title'=>__('Category List'))).'</li>';
 		echo '<li class="mod_tag">'.html::anchor('#','Tag List',array('title'=>__('Tag List'))).'</li>';
