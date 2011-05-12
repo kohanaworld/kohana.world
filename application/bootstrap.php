0f1f1c8a3ac720ed53e3aeed1a8d8ff1658d971e
biakaveron <biakaveron@gmail.com>
2011-05-12 13:19:00 +0400

    Cookie salt value required

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -55,6 +55,8 @@ ini_set('unserialize_callback_func', 'spl_autoload_call');
  */
 I18n::lang('en-us');
 
+Cookie::$salt = 'kohana world';
+
 /**
  * Set Kohana::$environment if a 'KOHANA_ENV' environment variable has been supplied.
  *
